30ab114c6c955023dad5162e97b6eed077ef7e3e
mmmeyers <marsi286@gmail.com>
2021-03-11 19:07:23 -0800

    commit tree e9f87937e9812b9126777a0957a1c1c3a4ccc1cd

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
-import Reviews from './Reviews';
+import React, { Component } from "react";
+import Reviews from "./Reviews";
 
 class ReviewInput extends Component {
+  state = {
+    text: "",
+  };
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    const review = {
+      text: this.state.text,
+      restaurantId: this.props.restaurantId,
+    };
+    this.props.addReview(review);
+    this.setState({
+      text: "",
+    });
+  };
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            value={this.state.text}
+            onChange={this.handleOnChange}
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
-};
+}
 
 export default ReviewInput;
