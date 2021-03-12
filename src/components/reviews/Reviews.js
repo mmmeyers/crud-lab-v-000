5efa03448ba3be09093d70e074d2c9e24b283a82
mmmeyers <marsi286@gmail.com>
2021-03-11 19:07:51 -0800

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import Review from './Review';
+import React, { Component } from "react";
+import Review from "./Review";
 
 class Reviews extends Component {
   render() {
     return (
       <ul>
-        Reviews
+        {this.props.reviews.map((review) => (
+          <Review
+            review={review}
+            key={review.id}
+            deleteReview={this.props.deleteReview}
+          />
+        ))}
       </ul>
     );
   }
-};
+}
 
 export default Reviews;
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import ReviewInput from '../components/reviews/ReviewInput';
-import Reviews from '../components/reviews/Reviews';
+import React, { Component } from "react";
+import ReviewInput from "../components/reviews/ReviewInput";
+import Reviews from "../components/reviews/Reviews";
+import { connect } from "react-redux";
 
 class ReviewsContainer extends Component {
+  filterReviews = () => {
+    return this.props.reviews.filter(
+      (review) => review.restaurantId === this.props.restaurant.id
+    );
+  };
 
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          addReview={this.props.addReview}
+          restaurantId={this.props.restaurant.id}
+        />
+        <Reviews
+          reviews={this.filterReviews()}
+          deleteReview={this.props.deleteReview}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default ReviewsContainer;
+const mapStateToProps = ({ reviews }) => ({ reviews });
+
+const mapDispatchToProps = (dispatch) => ({
+  addReview: (review) => dispatch({ type: "ADD_REVIEW", review }),
+  deleteReview: (id) => dispatch({ type: "DELETE_REVIEW", id }),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer);
